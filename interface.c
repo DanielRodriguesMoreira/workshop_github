f7ce87dfcfe769ac5efafe3cd70bfc999ec5c40d
DrMoreira <drmoreira@outlook.pt>
2017-05-03 19:05:55 +0100

    primeiro commit

---
+++ interface.c
@@ -0,0 +1,55 @@
+int main()
+
+{
+
+    int opcao;
+
+    printf("Bem vindo ao QUIZ!\n");
+
+
+
+    while (1)
+
+    {
+
+        printf("Escolha uma opção:\n");
+
+        printf("1 - Novo jogo\n");
+
+        printf("2 - Administrador\n");
+
+        printf("3 - Sair\n");
+
+
+
+        scanf("%d", &opcao);
+
+        if(opcao == 3)
+
+        {
+
+            break;
+
+        }
+
+
+
+        switch (opcao) {
+
+            case 1:
+
+                novo_jogo();
+
+                break;
+
+            case 2:
+
+                administrador();
+
+                break;
+
+        }
+
+    }
+
+}
